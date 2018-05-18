bc92f662976a78010259631c111397b8511761d6
Mark Mckessock <mark.mckessock@gmail.com>
2018-05-18 13:11:46 -0400

    quick sort

--- bubble_sort_complexity_05_16/bubble_sort_complexity_05_16/main.cpp
+++ bubble_sort_complexity_05_16/bubble_sort_complexity_05_16/main.cpp
@@ -4,8 +4,6 @@
 #include <random>
 #include <fstream>
 
-
-
 int rand_int(int low, int high) {
 	std::random_device rd; // obtain a random number from hardware
 	std::mt19937 eng(rd()); // seed the generator
@@ -21,33 +19,59 @@ std::vector<int> gen_vec(int n) {
 	return vals;
 }
 
-void bubble_sort(std::vector<int>& vals) {
-	int temp;
-	for (int i = 0; i < vals.size(); i++) {
-		for (int j = 0; j < vals.size() - i - 1; j++) {
-			if (vals[j] > vals[j + 1]) {
-				temp = vals[j];
-				vals[j] = vals[j + 1];
-				vals[j + 1] = temp;
+int partition(std::vector<int>& vals, int low, int high) {
+	int pivot = vals[low], hole = low;
+	bool increment = true;
+	while (high >= low) {
+		if (increment) {
+			if (vals[high] <= pivot) {
+				vals[hole] = vals[high];
+				vals[high] = pivot;
+				hole = high;
+				low++;
+				increment = false;
 			}
+			else
+				high--;
 		}
+		else {
+			if (vals[low] > pivot) {
+				vals[hole] = vals[low];
+				vals[low] = pivot;
+				hole = low;
+				increment = true;
+			}
+			else
+				low++;
+		}
+	}
+	return hole;
+}
+
+void quick_sort(std::vector<int>& vals, int low, int high) {
+	if (low < high) {
+		int pivot = partition(vals, low, high);
+		quick_sort(vals, low, pivot - 1);
+		quick_sort(vals, pivot + 1, high);
 	}
 }
 
 int main() {
+	std::string path = "C:/Users/Mark/Documents/times_1000.txt";
+	int increment = 10000;
 	std::clock_t begin,end;
 	std::vector<double> times;
 	std::ofstream file;
-	file.open("C:/Users/Mark/Documents/times_1000.txt");
-	for (int i = 0; i <= 10*10000; i+=10000) {
+	file.open(path);
+	for (int i = 0; i <= 10*increment; i+=increment) {
 		std::vector<int> vals = gen_vec(i);
 		std::cout << "Array Generated";
 		begin = clock();
-		bubble_sort(vals);
+		quick_sort(vals,0,vals.size()-1);
 		end = clock();
 		times.push_back(double(end - begin) / CLOCKS_PER_SEC);
-		std::cout << "N: " << i << "\tTime: " << times[i/10000] << std::endl;
-		file << "N: " << i << "\tTime: " << times[i/10000] << std::endl;
+		std::cout << "N: " << i << "\tTime: " << times[i/increment] << std::endl;
+		file << "N: " << i << "\tTime: " << times[i/increment] << std::endl;
 	}
 	system("pause");
 	file.close();
